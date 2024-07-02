9e96a513660045166ee94fd22fb9cf242adf098d
lokeshdev2103 <lokesh.dev2103@gmail.com>
2024-07-02 15:34:52 +0530

    basic concepts

---
+++ Basic.js
@@ -0,0 +1,83 @@
+// Javascript is scripting lanaguage 
+// Javascript is  synchronus  single -threaded lanaguage 
+
+const n = 3;
+const name = "puji"
+function square(num){
+    return num * num;
+}
+const square2 = square(n);
+const square3 = square(5);
+
+console.log(square2);
+console.log(square3);
+
+//step 1 
+
+// n = undefined
+// name = undefined
+// square = square(){...}
+// square2 = undefined
+// square3 = undefined
+
+// step 2 
+// n = 3;
+// name = "puji"
+// square = square(){..}
+// square2 = 9
+// square3 =
+
+
+// square method execution 
+// step 1 : 
+// num = undefined
+// num = 3
+// square = 9
+
+// step 1 
+// num = undefuned 
+// num = 5 
+// square3 = 25 
+
+// async 
+// await .then 
+// run at different time 
+
+// sync 
+// run at same time 
+
+//-----------------------------------------------------------------------------------
+
+// normal page application : 
+
+// www.puji.com (chrome search) --> backend --> server (request) 
+// server(resp) --> backend --> user(frontend)
+
+// chat --> backend -->server (request)
+// server(resp) --> backend --> user(puji chat)
+
+// frontend UI - html , css , javascript
+// backend  - c# 
+// database - mysql 
+
+
+// single page application 
+// www.puji.in ( chrome search) --> backend --> server(request)
+// server(resp) --> empty document --> user (frontend)--- this is called client based / single page 
+
+// frontend UI - vuejs 
+// backend  - c# 
+// database - mysql 
+
+// client side application 
+
+//---------------------------------------------------------------------------------------------------
+
+// code --> engine --> webpage
+   // 1 . parse code (orginal code) 
+   // 2. machine code (0101010)
+   // 3. excute 
+
+
+// chrome : v8 engine
+// firefox : spider monkey engine 
